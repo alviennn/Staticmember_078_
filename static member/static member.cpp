ceaca7a09f8babf73709bc1b35e40ab723f9cda1
alviennn <alvien.rnp@gmail.com>
2023-05-17 11:03:34 +0700

    menambahkan prosedur void untuk setID nim ++NIM dan menampilkan id dan nama

--- static member/static member.cpp
+++ static member/static member.cpp
@@ -15,5 +15,16 @@ public :
 
 int mahasiswa::nim = 0;
 
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+
 
 
